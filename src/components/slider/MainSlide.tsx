06d28379b8ba0f4e1218a7a384c6858e47a9e8d0
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-07-22 17:34:29 +0900

    style: swiper 커스텀

--- src/components/slider/MainSlide.tsx
+++ src/components/slider/MainSlide.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperOptions } from 'swiper/types/swiper-options';
 
 class SlideItem {
   src: string;
@@ -40,84 +40,88 @@ const slideList = [
   ),
   new SlideItem(
     'images/main-slider/slide4.jpg',
-    'Accessories with iphone',
+    'Accessories \nwith iphone',
     '아이폰과 함께 구매하면 좋은 액세서리'
   ),
   new SlideItem(
     'images/main-slider/slide5.jpg',
-    'Special deal 80% off',
+    'Special deal \n80% off',
     '토심이 토뭉이 다이어리를 단 1만 원에!',
     '재입고 특가!, 단 1만 원에 득템!'
   ),
   new SlideItem(
     'images/main-slider/slide6.png',
-    'Special offer',
+    'Special \noffer',
     '곽철이 피크닉 매트 재입고 특가',
     '1만 원대 득템 찬스!, 특별한 나들이템'
   ),
   new SlideItem(
     'images/main-slider/slide7.jpg',
-    'Ready for the cool summer',
+    'Ready for \nthe cool summer',
     '카카오프렌즈 여름 특별 할인전 Open',
     '선착순 300명!, 사은품 제공'
   ),
   new SlideItem(
     'images/main-slider/slide8.jpg',
-    'Jeongwaja edition',
+    'Jeongwaja \nedition',
     '티다문구점 x 전과자 베이비두부 에디션'
   ),
   new SlideItem(
     'images/main-slider/slide9.jpg',
-    'Digital type test',
+    'Digital \ntype test',
     '테스트하고 디지털 부적 받기'
   ),
   new SlideItem(
     'images/main-slider/slide10.gif',
-    'Digital life T gift zone',
+    'Digital life \nT gift zone',
     '인터넷 / Btv 가입하고 티다문구점 이용권 받았다면?'
   ),
   new SlideItem(
     'images/main-slider/slide11.gif',
-    'We can buy all for lovers',
+    'We can buy all \nfor lovers',
     '깔끔하게 만족스러운 Big sale 2탄'
   ),
   new SlideItem(
     'images/main-slider/slide12.jpg',
-    'Tida T gift zone',
+    'Tida \nT gift zone',
     'T기프트(티다문구점 이용권)로 액세서리 알뜰하게 구매하기',
     'T 기프트 받았다면?'
   ),
 ];
 
-const MainSlide = () => {
-  // useEffect(() => {}, []);
-  // const swiper = new Swiper('.main-slider', {
-  //   modules: [Navigation, Pagination],
-  //   loop: true,
-  // });
+const swiperOption: SwiperOptions = {
+  modules: [Autoplay, Pagination],
+  pagination: true,
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: false,
+  },
+  speed: 1000,
+  loop: true,
+};
 
+const MainSlide = () => {
   return (
     <section className='main-slider'>
       <h2 className='blind'>메인 슬라이드 영역</h2>
-      {/* <div className='main-slider__slide swiper-slide'> */}
-      <Swiper className='main-slider__slide-container'>
+      <Swiper className='main-slider__slide-container' {...swiperOption}>
         {slideList.map((el, idx) => (
           <SwiperSlide className='main-slider__slide'>
             <a href='#'>
-              <img src={el.src} alt={el.alt || ''} />
-              <h3 className='main-slider__title'>{el.title}</h3>
-              <p className='main-slider__desc'>{el.desc}</p>
+              <img
+                className={`main-slider__img`}
+                src={el.src}
+                alt={el.alt || ''}
+              />
+              <h3 className={`main-slider__title ${idx <= 2 ? 'blind' : ''}`}>
+                {el.title}
+              </h3>
+              <p className={`main-slider__desc ${idx <= 2 ? 'blind' : ''}`}>
+                {el.desc}
+              </p>
             </a>
           </SwiperSlide>
         ))}
-        {/* <SwiperSlide className='main-slider__slide'>
-          <a href='#'>
-            <h3 className='main-slider__title'>Special 80% off</h3>
-            <p className='main-slider__desc'>
-              토심이 토뭉이 다이어리를 단 1만 원에!
-            </p>
-          </a>
-        </SwiperSlide> */}
       </Swiper>
     </section>
   );
