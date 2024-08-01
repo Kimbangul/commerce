dbb2a7064fcd9c7193cc28c9945becac219158ca
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-08-02 06:48:14 +0900

    style: favicon 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <title>티다문구점</title>
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
